160df28471302455812087f815fc94e1ec5ebb2f
martinolega <martinolega11@gmail.com>
2025-11-04 14:21:22 +0100

    feat:snack-2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const createArrayStartingWith = (array, character) =>
+{
+    const result = [];
+
+    for(let i = 0; i < array.length; i++)
+    {
+        if(array[i].charAt(0).toLowerCase() === character.toLowerCase())
+        {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const arrayStartingWith = createArrayStartingWith(names, "a");
+console.log(arrayStartingWith);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
