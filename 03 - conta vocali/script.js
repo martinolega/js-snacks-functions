1f8548a6d251439a92f416dc6801bfb4d41b707c
martinolega <martinolega11@gmail.com>
2025-11-04 14:33:14 +0100

    feat:snack-3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,38 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const isVowel = (letter) =>
+{
+    const tested = letter.toLowerCase();
+
+    if(tested === 'a' || tested === 'e' || tested === 'i' || tested === 'o' || tested == 'u')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+const numberOfVowels = (string) =>
+{
+    let result = 0;
+
+    for(let i = 0; i < string.length; i++)
+    {
+        if(isVowel(string[i]))
+        {
+            result++;
+        }
+    }
+
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const numVowels = numberOfVowels(word);
+console.log(numVowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
