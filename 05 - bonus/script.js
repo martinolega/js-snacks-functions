14badbb7ea09efe8a46676835f329b70fa4e84b7
martinolega <martinolega11@gmail.com>
2025-11-04 14:44:11 +0100

    feat:snack-5

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -10,9 +10,33 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+const saluta = (nome) =>
+{
+    let saluto = "";
+    const date = new Date();
+    let hour = date.getHours();
+
+    if(hour > 3 && hour <= 13)
+    {
+        saluto = "Buongiorno ";
+    }
+    else if(hour <= 17)
+    {
+        saluto = "Buon pomeriggio ";
+    }
+    else
+    {
+        saluto = "Buonasera ";
+    }
+
+    saluto = saluto + nome;
+
+    return saluto;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(saluta(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
